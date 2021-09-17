5b90a49a40056e80914240ba483076da238e5719
AtulYadav01 <ashuy033@gmail.com>
2021-09-17 15:51:31 +0530

    Initial commit

---
+++ app/src/main/java/com/e/movabletext/MainActivity.kt
@@ -0,0 +1,135 @@
+package com.e.movabletext
+
+import android.annotation.SuppressLint
+import android.graphics.PointF
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.view.View
+import android.widget.TextView
+import kotlin.math.atan2
+import kotlin.math.sqrt
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textView: TextView
+    private var lastEvent: FloatArray? = null
+    private var d = 0f
+    private var newRot = 0f
+    private var isZoomAndRotate = false
+    private var isOutSide = false
+    private var mode = NONE
+    private val start = PointF()
+    private val mid = PointF()
+    private var oldDist = 1f
+    private var xCoOrdinate = 0f
+    private var yCoOrdinate = 0f
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.textViewId)
+
+        textView.setOnTouchListener { v, event ->
+            val view = v as TextView
+            view.bringToFront()
+            viewTransformation(view, event)
+            true
+        }
+    }
+
+    private fun viewTransformation(view: View, event: MotionEvent) {
+        when (event.action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {
+                xCoOrdinate = view.x - event.rawX
+                yCoOrdinate = view.y - event.rawY
+                start[event.x] = event.y
+                isOutSide = false
+                mode = DRAG
+                lastEvent = null
+            }
+            MotionEvent.ACTION_POINTER_DOWN -> {
+                oldDist = spacing(event)
+                if (oldDist > 10f) {
+                    midPoint(mid, event)
+                    mode = ZOOM
+                }
+                lastEvent = FloatArray(4)
+                lastEvent!![0] = event.getX(0)
+                lastEvent!![1] = event.getX(1)
+                lastEvent!![2] = event.getY(0)
+                lastEvent!![3] = event.getY(1)
+                d = rotation(event)
+            }
+            MotionEvent.ACTION_UP -> {
+                isZoomAndRotate = false
+                if (mode == DRAG) {
+                    event.x
+                    event.y
+                }
+                isOutSide = true
+                mode = NONE
+                lastEvent = null
+                mode = NONE
+                lastEvent = null
+            }
+            MotionEvent.ACTION_OUTSIDE -> {
+                isOutSide = true
+                mode = NONE
+                lastEvent = null
+                mode = NONE
+                lastEvent = null
+            }
+            MotionEvent.ACTION_POINTER_UP -> {
+                mode = NONE
+                lastEvent = null
+            }
+            MotionEvent.ACTION_MOVE -> if (!isOutSide) {
+                if (mode == DRAG) {
+                    isZoomAndRotate = false
+                    view.animate().x(event.rawX + xCoOrdinate).y(event.rawY + yCoOrdinate)
+                        .setDuration(0).start()
+                }
+                if (mode == ZOOM && event.pointerCount == 2) {
+                    val newDist1 = spacing(event)
+                    if (newDist1 > 10f) {
+                        val scale = newDist1 / oldDist * view.scaleX
+                        view.scaleX = scale
+                        view.scaleY = scale
+                    }
+                    if (lastEvent != null) {
+                        newRot = rotation(event)
+                        view.rotation = (view.rotation + (newRot - d))
+                    }
+                }
+            }
+        }
+    }
+
+    private fun rotation(event: MotionEvent): Float {
+        val delta_x = (event.getX(0) - event.getX(1)).toDouble()
+        val delta_y = (event.getY(0) - event.getY(1)).toDouble()
+        val radians = atan2(delta_y, delta_x)
+        return Math.toDegrees(radians).toFloat()
+    }
+
+    private fun spacing(event: MotionEvent): Float {
+        val x = event.getX(0) - event.getX(1)
+        val y = event.getY(0) - event.getY(1)
+        return sqrt((x * x + y * y).toDouble()).toFloat()
+    }
+
+    private fun midPoint(point: PointF, event: MotionEvent) {
+        val x = event.getX(0) + event.getX(1)
+        val y = event.getY(0) + event.getY(1)
+        point[x / 2] = y / 2
+    }
+
+    companion object {
+        private const val NONE = 0
+        private const val DRAG = 1
+        private const val ZOOM = 2
+    }
+}
